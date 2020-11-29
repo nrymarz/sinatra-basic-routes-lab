7eeba15857156ff02bd05b2c413670dacd8b4d9a
nrymarz <nrymarz@gmail.com>
2020-11-29 14:56:27 -0600

    commit tree 0219711d048efd5678e7cd6a89bd82ffc926aca3

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+    
+    get '/favorite-song' do
+        "My favorite song is Cause We've Ended as Lovers"
+    end
+    
+    get '/hometown' do
+        "My hometown is Barrington"
+    end
+    
+    get '/name' do
+        "My name is Nathan"
+    end 
 end
